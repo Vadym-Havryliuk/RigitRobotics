ade3dadeccf5a1a0b89945a8ee73cea2ea421fd2
Vadym Havryliuk <vadimg692@gmail.com>
2023-11-21 23:47:36 +0200

    commit-8

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 "use strict";
 
-let body = document.getElementsByTagName('body')[0];
-let menu = document.querySelector('.nav-content .open-links');
+let body = document.body;
+let menu = document.querySelector('.links-navigation');
+let logo = document.querySelector('.nav-content .logo');
+let square2 = document.querySelector('.header .square-copy-2');
+
+function changePosition() {
+    if (window.innerWidth < 1050) {
+        square2.after(menu);
+    } else {
+        logo.after(menu);   
+    }
+}
+
+changePosition();
+
+addEventListener('resize', changePosition);
