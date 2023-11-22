a06bc2febed19a850a57f01dc10ed208060d7920
Vadym Havryliuk <vadimg692@gmail.com>
2023-11-22 03:36:48 +0200

    commit-10

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ let button = document.querySelector('.button-contact');
 let buttonShadow = document.querySelector('.button-shadow');
 let header = document.querySelector('.header');
 let aside = document.querySelector('.aside');
+let openLinks = document.querySelector('.open-links');
 
 function changePosition() {
     if (window.innerWidth < 1050) {
-        logo.after(menu);
+        header.append(menu);
 
         if (window.innerWidth < 400) { 
             header.after(button);
@@ -24,6 +25,21 @@ function changePosition() {
     }
 }
 
+function transitionNav() {
+    if (window.innerWidth < 1050) {
+        switch (menu.style.top) {
+            case '-138px':
+                menu.style.top = '102px'; 
+                break;
+            case '102px':
+                menu.style.top = '-138px';
+                break;
+        }
+             
+    }
+}
+
 changePosition();
 
 addEventListener('resize', changePosition);
+openLinks.addEventListener('click', transitionNav);
