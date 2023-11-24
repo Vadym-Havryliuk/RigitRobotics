749e07bbc66c4d05474c57d97140ad25fed667fd
Vadym Havryliuk <vadimg692@gmail.com>
2023-11-24 23:14:17 +0200

    commit-11

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 let body = document.body;
 let menu = document.querySelector('.links-navigation');
+let whiteBlock = document.querySelector('.white-block');
+
 let logo = document.querySelector('.nav-content .logo');
 let button = document.querySelector('.button-contact');
 let buttonShadow = document.querySelector('.button-shadow');
@@ -25,21 +27,93 @@ function changePosition() {
     }
 }
 
-function transitionNav() {
-    if (window.innerWidth < 1050) {
-        switch (menu.style.top) {
-            case '-138px':
-                menu.style.top = '102px'; 
-                break;
-            case '102px':
-                menu.style.top = '-138px';
-                break;
-        }
-             
+function getHeight() {
+    if (window.innerWidth > 840) {
+        return 1;
+    } else if (window.innerWidth > 715) {
+        return 2;
+    } else if (window.innerWidth > 550) {
+        return 3;
+    }
+}
+
+function assignHeight() {
+    switch(getHeight()) {
+        case 1:
+            return '102px';
+        break;
+        case 2:
+            return '82px';
+        break;
+        case 3:
+            return '62px';
+        break;
+    }
+}
+
+function assignBottom() {
+    switch(getHeight()) {
+        case 1:
+            return '-138px';
+        break;
+        case 2:
+            return '-158px';
+        break;
+        case 3:
+            return '-178px';
+        break;
+    }
+}
+
+function transitionNavBottom() {
+    if (window.innerWidth < 1050 && window.innerWidth > 550) {
+        menu.style.animationName = `to-bottom-${getHeight()}`;
+        menu.style.animationDuration = '1s';
+        menu.style.animationFillMode = 'forwards';
+
+        whiteBlock.style.animationName = `to-bottom-${getHeight()}`;
+        whiteBlock.style.animationDuration = '1s';
+        whiteBlock.style.animationFillMode = 'forwards';
+        
+        openLinks.removeEventListener('click', transitionNavBottom);
+
+        setTimeout(() => {
+            menu.style.top = assignHeight();
+            whiteBlock.style.top = assignHeight();
+            whiteBlock.style.boxShadow = '0px 0px 20px 10px white';
+
+            openLinks.addEventListener('click', transitionNavTop); 
+        }, 1100);
+           
+    }
+}
+
+function transitionNavTop() {
+    if (window.innerWidth < 1050 && window.innerWidth > 550) {
+        whiteBlock.style.boxShadow = '0px 0px 0px 0px white';
+
+        menu.style.animationName = `to-top-${getHeight()}`;
+        menu.style.animationDuration = '1s';
+        menu.style.animationDelay = '100ms';
+        menu.style.animationFillMode = 'forwards';
+
+        whiteBlock.style.animationName = `to-top-${getHeight()}`;
+        whiteBlock.style.animationDuration = '1s';
+        whiteBlock.style.animationDelay = '100ms';
+        whiteBlock.style.animationFillMode = 'forwards';
+
+        openLinks.removeEventListener('click', transitionNavTop);
+
+        setTimeout(() => {
+            menu.style.top = assignBottom();
+            whiteBlock.style.top = assignBottom();
+
+            openLinks.addEventListener('click', transitionNavBottom);
+        }, 1100);
     }
 }
 
 changePosition();
 
 addEventListener('resize', changePosition);
-openLinks.addEventListener('click', transitionNav);
+openLinks.addEventListener('click', transitionNavBottom);
